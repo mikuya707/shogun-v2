6794ab774e1dc5b8eb42fd7c692ebe97d87bbd06
jl975 <jlee6891@gmail.com>
2015-10-30 23:09:18 -0400

    another shot at conditional env

--- src/js/io.js
+++ src/js/io.js
@@ -1,26 +1,10 @@
-// // 'use strict';
-
-// import io from 'socket.io-client';
-// const ORIGIN = 'http://localhost:1337';
-// const WS = ORIGIN;
-
-// export default io.connect(WS);
+'use strict';
 
 import io from 'socket.io-client';
 import os from  "os";
 const hostname = os.hostname();
-var ENV = process.env.NODE_ENV;
-const ORIGIN;
 
 const port = process.env.PORT || 1337;
-if(ENV === 'development'){
- 	ORIGIN = `${hostname}:${port}`; 
-}
-else{
-	ORIGIN = `${hostname}`;
-}
+const ORIGIN = hostname.includes('herokuapp.com') ? hostname : hostname+":"+port;
 
-
-// const HOST = `localhost:${process.env.PORT}`;
- console.log('what is ORIGIN', ORIGIN);
 export default io.connect(ORIGIN);
