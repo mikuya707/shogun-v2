94cf6c8dd877e67989204bde9b0c20ebd4feacde
Karen Mao <karen.mao707@gmail.com>
2015-10-30 20:52:05 -0400

    socket issue

--- src/js/play.js
+++ src/js/play.js
@@ -6,6 +6,7 @@ import io from './io';
 import GameInterface from './components/GameInterface';
 
 let params = window.location.pathname.replace('/play/', '').split('/');
+console.log("what is params", params);
 params[1] = parseInt(params[1], 10);
 params[2] = parseInt(params[2], 10);
 
