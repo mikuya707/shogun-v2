ba5f7f477c197ac6098da336073db89491de0d2a
Karen Mao <karen.mao707@gmail.com>
2015-10-30 20:04:54 -0400

    trying to fix socket issue

--- src/js/io.js
+++ src/js/io.js
@@ -1,7 +1,14 @@
-'use strict';
+// // 'use strict';
+
+// import io from 'socket.io-client';
+// const ORIGIN = 'http://localhost:1337';
+// const WS = ORIGIN;
+
+// export default io.connect(WS);
 
 import io from 'socket.io-client';
-const ORIGIN = 'http://localhost:1337';
-const WS = ORIGIN;
 
-export default io.connect(WS);
+
+const HOST = 'http://localhost:1337';
+
+export default io.connect(HOST);
