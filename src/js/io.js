adfc8b4539371e980a85d7bad13604fc8ce321f0
jl975 <jlee6891@gmail.com>
2015-11-05 17:25:16 -0500

    ORIGIN: replace includes with indexOf

--- src/js/io.js
+++ src/js/io.js
@@ -4,7 +4,9 @@ import io from 'socket.io-client';
 import os from  "os";
 const hostname = os.hostname();
 
+console.log(hostname);
+
 const port = process.env.PORT || 1337;
-const ORIGIN = hostname.includes('herokuapp.com') ? hostname : hostname+":"+port;
+const ORIGIN = hostname.indexOf('herokuapp.com') !== -1 ? hostname : hostname+":"+port;
 
 export default io.connect(ORIGIN);
